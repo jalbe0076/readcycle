639e834648d0db107aaa47a28368fb4ee7459894
Jason Alberto <alberto.jason@gmail.com>
2023-12-04 14:54:04 -0500

    Format and display searched books

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import { getBooks } from '../../apiCalls';
+
+interface Book {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishedDate: string;
+    imageLinks: {smallThumbnail: string}
+  }
+}
 
 function App() {
+  const [searchResults, setSearchResults] = useState<Book[]>([]);
+  const [totalResults, setTotalResults] = useState<number>(0);
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const data = await getBooks('harry potter');
+        console.log(data.items)
+        data && setSearchResults(data.items);
+        data && setTotalResults(data.totalItems);
+      } catch (error) {
+        console.log(error)
+      }
+    })();
+  },[]);
+
+  const books = searchResults.map(book => {
+    const {title, authors, publisher, imageLinks } = book.volumeInfo;
+    console.log(imageLinks)
+    return (
+      <article>
+        <img src={imageLinks.smallThumbnail}/>
+        <h2>{title}</h2>
+      </article>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {books}
     </div>
   );
 }
